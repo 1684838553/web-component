17a20fbc6ba68671e8c9fb04cbf05e4779ca30a7
1684838553 <1684838553@qq.com>
2022-07-03 22:00:28 +0800

    badge组件status属性

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import Badge1 from "./badge/badge";
 import { Avatar, Badge } from "antd";
 import "./app.css";
+import { ClockCircleOutlined } from "@ant-design/icons";
 
 function App() {
   return (
     <div className="container">
-      <Badge count={444} color="blue" title="blue" dot>
+      <Badge
+        count={
+          // <ClockCircleOutlined
+          //   style={{
+          //     color: "#f5222d",
+          //   }}
+          // />
+          19
+        }
+        // offset={["10", 10]}
+        color="red"
+        // title="blue"
+        // size="small"
+        // dot={true}
+        status="warning"
+      >
         {/* <div>gggg</div> */}
 
-        <Avatar shape="square" size="large" />
+        {/* <Avatar shape="square" size="large" /> */}
+        {/* <HomeOutlined /> */}
       </Badge>
 
       <div className="badge-content">
-        <Badge1 count={222} color="blue" title="blue" dot={true}>
+        <Badge1
+          count={6}
+          color="red"
+          title="blue"
+          size="small"
+          dot
+          offset={[10, 10]}
+          status="warning"
+        >
           {/* <div>gggg</div> */}
-          <Avatar shape="square" size="large" />
+          {/* <Avatar shape="square" size="large" /> */}
         </Badge1>
       </div>
     </div>
--- src/badge/badge.js
+++ src/badge/badge.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import "./badge.css";
 
 export default function Badge1(props) {
@@ -17,43 +17,80 @@ export default function Badge1(props) {
   } = props;
 
   const [showCount, setShowCount] = useState(count);
-  const [, setShowDot] = useState(dot);
+  const [showDot, setShowDot] = useState(dot);
   const [UIBadgeStyle, setUIBadgeStyle] = useState({});
+  const [backgroundColor, setBackgroundColor] = useState(color);
 
   useEffect(() => {
-    if (count > overflowCount) {
+    // count 类型为number时 超过定义的最大值，展示 最大值+
+    if (typeof count === "number" && count > overflowCount) {
       setShowCount(`${overflowCount}+`);
+    } else {
+      // 如果 count 是一个元素节点，设置的 背景色无效
+      setBackgroundColor(undefined);
     }
 
-    if (color && !children) {
+    // 设置颜色，并且没有传children ，dot 为true
+
+    // 状态 ： 设置状态属性，传入children，status属性失效
+
+    if ((color || status) && !children) {
       setShowDot(true);
     }
 
-    if (color) {
+    // count 时图标时，直接展示图标，设置的其他属性，如颜色，dot,status,size失效
+    // offset={[10, 10]} 两个参数，number或string类型
+    if (color && typeof count !== "object") {
+      // 修改 badge 背景色
       setUIBadgeStyle({
         ...UIBadgeStyle,
-        background: color,
+        background: backgroundColor,
+        marginTop: offset[0],
+        marginRight: offset[1],
       });
     }
   }, [props]);
+
+  const mergedStyle = useMemo(() => {
+    if (!offset) {
+      return UIBadgeStyle;
+    }
+
+    const offsetStyle = { marginTop: offset[1] };
+    offsetStyle.right = -parseInt(offset[0], 10);
+
+    return {
+      ...offsetStyle,
+      ...UIBadgeStyle,
+    };
+  }, [offset, UIBadgeStyle]);
 
   return (
     <span className="ui-badge">
       {children}
       {!dot ? (
         <span
-          className="ui-badge-count"
+          className={[
+            typeof showCount === "object"
+              ? "ui-badge-count-icon"
+              : "ui-badge-count",
+            // dot 为 true时，size失效
+            size === "small" && !showDot ? "ui-badge-count-sm" : "",
+          ].join(" ")}
           title={title ?? count}
-          style={UIBadgeStyle}
+          style={{ ...UIBadgeStyle, ...mergedStyle }}
         >
           {showCount}
         </span>
       ) : null}
       {dot ? (
         <span
-          className="ui-badge-dot"
+          className={[
+            "ui-badge-dot",
+            status ? `ui-badge-status-${status}` : "",
+          ].join(" ")}
           title={title ?? count}
-          style={UIBadgeStyle}
+          style={{ ...UIBadgeStyle, ...mergedStyle }}
         ></span>
       ) : null}
     </span>
